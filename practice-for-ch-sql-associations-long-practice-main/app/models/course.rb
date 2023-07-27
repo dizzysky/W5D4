d8028cf46c49c965e0e78bb54dda21acbef6e0ef
Sanjid <dsanjid@gmail.com>
2023-07-27 15:06:31 -0400

    some associations

--- practice-for-ch-sql-associations-long-practice-main/app/models/course.rb
+++ practice-for-ch-sql-associations-long-practice-main/app/models/course.rb
@@ -1,2 +1,36 @@
+# == Schema Information
+#
+# Table name: courses
+#
+#  id            :bigint           not null, primary key
+#  name          :string           not null
+#  prereq_id     :bigint
+#  instructor_id :bigint           not null
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
 class Course < ApplicationRecord
+
+
+    # belongs_to :prereq,
+    # primary_key: :id, 
+    # foreign_key: :prereq_id, 
+    # class: 
+
+
+    has_many :enrollments, 
+    primary_key: :id, 
+    foreign_key: :course_id, 
+    class_name: :Enrollment
+
+
+    has_many :students, 
+    through: :enrollments, 
+    source: :user
+
+
+
+
+
+    
 end
